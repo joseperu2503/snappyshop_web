a244ff1736f4edd228e5d25d4e2dcdf689e7b05c
joseperu2503 <joseperu2503@gmail.com>
2024-07-31 15:21:56 -0500

    custom input

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -29,24 +29,29 @@ import {
 })
 export class InputComponent implements ControlValueAccessor {
   @Input() label?: string;
+  @Input() value?: string;
   @Input() placeholder: string = '';
   @Input() formControlName: string = '';
+  @Input() errors = {};
+  @Input() disabled: boolean = false;
+
+  get innerErrors() {
+    return {
+      required: 'This field is required',
+      email: 'Invalid email format',
+      ...this.errors,
+    };
+  }
 
   constructor(
     @Optional() @Host() @SkipSelf() private controlContainer: ControlContainer
   ) {}
 
-  private innerValue: any = '';
-  disabled: boolean = false;
   // Function to call when the value changes
-  onChange: any = () => {};
+  onChange: Function = () => {};
 
   // Function to call when the input is touched
-  onTouched: any = () => {};
-
-  get value(): any {
-    return this.innerValue;
-  }
+  onTouched: Function = () => {};
 
   get control(): FormControl {
     return this.controlContainer?.control?.get(
@@ -58,45 +63,35 @@ export class InputComponent implements ControlValueAccessor {
     const control = this.control;
     if (control && control.touched && control.errors) {
       if (control.errors['required']) {
-        return 'This field is required';
+        return this.innerErrors.required;
       }
-      if (control.errors['minlength']) {
-        return `Minimum length is ${control.errors['minlength'].requiredLength}`;
-      }
-
       if (control.errors['email']) {
-        return 'Invalid email format';
+        return this.innerErrors.email;
       }
     }
     return null;
   }
 
-  set value(val: any) {
-    if (val !== this.innerValue) {
-      this.innerValue = val;
-      this.onChange(val);
-    }
+  writeValue(value: string): void {
+    this.value = value;
   }
 
-  writeValue(value: any): void {
-    this.innerValue = value;
-  }
-
-  registerOnChange(fn: any): void {
+  registerOnChange(fn: Function): void {
     this.onChange = fn;
   }
 
-  registerOnTouched(fn: any): void {
+  registerOnTouched(fn: Function): void {
     this.onTouched = fn;
   }
 
-  setDisabledState?(isDisabled: boolean): void {
+  setDisabledState(isDisabled: boolean): void {
     this.disabled = isDisabled;
   }
 
-  // Handling input events
-  handleInput(event: any): void {
-    this.value = event.target.value;
+  handleInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    this.value = input.value;
+    this.onChange(this.value);
   }
 
   handleBlur(): void {
