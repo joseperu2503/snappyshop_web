ca07b854c50a9cebf3f16d4cd31934c23bec143b
joseperu2503 <joseperu2503@gmail.com>
2024-08-02 18:58:48 -0500

    infinit scroll

---
+++ src/app/core/enums/loading-status.enum.ts
@@ -0,0 +1,6 @@
+export enum LoadingStatus {
+  None,
+  Loading,
+  Sucess,
+  Error,
+}
--- src/app/features/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { ProductService } from '../../../product/services/product.service';
 import { Product } from '../../../product/dtos/products-response.dto';
 import { ProductItemComponent } from '../../components/product-item/product-item.component';
+import { LoadingStatus } from '../../../../core/enums/loading-status.enum';
+import { UtilService } from '../../../../shared/services/util/util.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,14 +14,58 @@ import { ProductItemComponent } from '../../components/product-item/product-item
 })
 export default class DashboardComponent {
   private productService = inject(ProductService);
+  private utilService = inject(UtilService);
 
   products: Product[] = [];
+  page: number = 1;
+  totalPages: number = 1;
+  loading: LoadingStatus = LoadingStatus.None;
 
   ngOnInit() {
-    this.productService.getProducts().subscribe({
-      next: (response) => {
-        this.products = response.results;
-      },
-    });
+    this.getProducts();
+  }
+
+  getProducts() {
+    if (this.page > this.totalPages) return;
+    if (this.loading == LoadingStatus.Loading) return;
+
+    this.loading = LoadingStatus.Loading;
+    this.productService
+      .getProducts({
+        page: this.page,
+      })
+      .subscribe({
+        next: (response) => {
+          this.page = this.page + 1;
+          this.totalPages = response.info.last_page;
+          this.products.push(...response.results);
+          this.loading = LoadingStatus.Sucess;
+        },
+        error: (error) => {
+          this.utilService.openSnackBar(
+            'An error occurred while loading the products.'
+          );
+          this.loading = LoadingStatus.Error;
+        },
+      });
+  }
+
+  ngAfterViewInit(): void {
+    window.addEventListener('scroll', () => this.handleScroll());
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', () => this.handleScroll());
+  }
+
+  handleScroll() {
+    const pageHeight = document.documentElement.scrollHeight;
+    const windowHeight = window.innerHeight;
+    const scrollPosition = window.scrollY;
+    const distanceFromBottom = 300;
+    const triggerPosition = pageHeight - windowHeight - distanceFromBottom;
+    if (scrollPosition >= triggerPosition) {
+      this.getProducts();
+    }
   }
 }
