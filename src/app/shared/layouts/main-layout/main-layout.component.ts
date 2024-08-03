ef143ac843905403f0b72a83c7cb46aab20032eb
joseperu2503 <joseperu2503@gmail.com>
2024-08-02 23:59:34 -0500

    avance estilos

--- src/app/shared/layouts/main-layout/main-layout.component.ts
+++ src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { SearchInputComponent } from '../../../features/dashboard/components/search-input/search-input.component';
 import { SvgIconComponent } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [SearchInputComponent, SvgIconComponent, RouterOutlet],
+  imports: [SearchInputComponent, SvgIconComponent, RouterOutlet, RouterLink],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
