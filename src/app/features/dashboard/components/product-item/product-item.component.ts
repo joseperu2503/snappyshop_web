a843e1c284405e4b5f7c326d96c6dddacf7f668d
joseperu2503 <joseperu2503@gmail.com>
2024-08-02 23:59:34 -0500

    avance estilos

--- src/app/features/dashboard/components/product-item/product-item.component.ts
+++ src/app/features/dashboard/components/product-item/product-item.component.ts
@@ -1,13 +1,31 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { Product } from '../../../product/dtos/products-response.dto';
+import { RouterLink } from '@angular/router';
+import { PricePipe } from '../../../../core/pipes/price/price.pipe';
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, PricePipe],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss',
 })
 export class ProductItemComponent {
   public product = input.required<Product>();
+
+  basePrice = computed<number | null>(() => {
+    if (!this.product()) return null;
+    if (!this.product()!.discount) {
+      return null;
+    }
+    return this.product()!.price;
+  });
+
+  salePrice = computed<number | null>(() => {
+    if (!this.product()) return null;
+    if (!this.product()!.discount) {
+      return this.product()!.price;
+    }
+    return this.product()!.price * (1 - this.product()!.discount / 100);
+  });
 }
