dbd7d2cec923354073fe84ebff61e1888897ab0b
joseperu2503 <joseperu2503@gmail.com>
2024-08-02 23:40:25 -0500

    avance product page

--- src/app/features/product/pages/product/product.component.ts
+++ src/app/features/product/pages/product/product.component.ts
@@ -1,13 +1,22 @@
-import { Component, Input, inject, input, signal } from '@angular/core';
+import {
+  Component,
+  Input,
+  computed,
+  inject,
+  input,
+  signal,
+} from '@angular/core';
 import { LoadingStatus } from '../../../../core/enums/loading-status.enum';
 import { ProductService } from '../../services/product.service';
 import { UtilService } from '../../../../shared/services/util/util.service';
 import { Product } from '../../dtos/products-response.dto';
+import { PricePipe } from '../../../../core/pipes/price/price.pipe';
+import { ButtonComponent } from '../../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [PricePipe, ButtonComponent],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
 })
@@ -21,6 +30,22 @@ export default class ProductComponent {
 
   loading = signal<LoadingStatus>(LoadingStatus.None);
   product = signal<Product | null>(null);
+
+  basePrice = computed<number | null>(() => {
+    if (!this.product()) return null;
+    if (!this.product()!.discount) {
+      return null;
+    }
+    return this.product()!.price;
+  });
+
+  salePrice = computed<number | null>(() => {
+    if (!this.product()) return null;
+    if (!this.product()!.discount) {
+      return this.product()!.price;
+    }
+    return this.product()!.price * (1 - this.product()!.discount / 100);
+  });
 
   ngOnInit() {
     this.getProduct();
