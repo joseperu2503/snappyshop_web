7b9f5ff823319d7c4c4df0924cb7e50f3bd62188
joseperu2503 <joseperu2503@gmail.com>
2024-08-06 14:55:09 -0500

    scroll config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,32 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+  InMemoryScrollingOptions,
+  provideRouter,
+  withComponentInputBinding,
+  InMemoryScrollingFeature,
+  withInMemoryScrolling,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideAngularSvgIcon } from 'angular-svg-icon';
 
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled',
+};
+const inMemoryScrollingFeature: InMemoryScrollingFeature =
+  withInMemoryScrolling(scrollConfig);
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      inMemoryScrollingFeature
+    ),
     provideHttpClient(),
     provideAnimationsAsync(),
     provideAngularSvgIcon(),
