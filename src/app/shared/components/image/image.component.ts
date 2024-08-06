b8f0a4ce7b62a4cc91904dc1be6b41e3e2b02cb6
joseperu2503 <joseperu2503@gmail.com>
2024-08-06 18:18:42 -0500

    image zoom

--- src/app/shared/components/image/image.component.ts
+++ src/app/shared/components/image/image.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-image',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './image.component.html',
   styleUrl: './image.component.scss',
 })
 export class ImageComponent {
   public src = input.required<string | null | undefined>();
+  public withZoom = input<boolean>(false);
 }
