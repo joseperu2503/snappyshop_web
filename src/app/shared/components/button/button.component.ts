45f39a5040c30cbd2b0d4960018683d27744e289
joseperu2503 <joseperu2503@gmail.com>
2024-08-06 12:25:30 -0500

    product detail

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,16 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [MatProgressSpinnerModule, MatButtonModule],
+  imports: [MatProgressSpinnerModule, MatButtonModule, CommonModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
-  @Input() disabled: boolean = false;
-  @Input() loading: boolean = false;
-  @Input() text: string = '';
+  disabled = input<boolean>(false);
+  loading = input<boolean>(false);
+  text = input<string>('');
+  type = input<'primary' | 'secondary'>('primary');
 }
