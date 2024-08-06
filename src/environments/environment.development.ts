107555ebaa8e5397faa28128db3af18d08cd47a7
joseperu2503 <joseperu2503@gmail.com>
2024-08-05 19:13:02 -0500

    estilos

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  URL_BASE: 'https://snappyshop-api.joseperezgil.com/api',
+  // URL_BASE: 'https://snappyshop-api.joseperezgil.com/api',
+  URL_BASE:
+    'https://a874-38-253-148-190.ngrok-free.app/snappyshop_backend/public/api',
 };
