33a3efb56d4453999c75f4850eb5f434b0a7470a
joseperu2503 <joseperu2503@gmail.com>
2024-08-05 20:50:24 -0500

    swiper

--- src/app/features/product/pages/product/product.component.ts
+++ src/app/features/product/pages/product/product.component.ts
@@ -1,6 +1,8 @@
 import {
+  CUSTOM_ELEMENTS_SCHEMA,
   Component,
-  Input,
+  ElementRef,
+  ViewChild,
   computed,
   inject,
   input,
@@ -12,17 +14,24 @@ import { UtilService } from '../../../../shared/services/util/util.service';
 import { Product } from '../../dtos/products-response.dto';
 import { PricePipe } from '../../../../core/pipes/price/price.pipe';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
+import { ImageComponent } from '../../../../shared/components/image/image.component';
+import { SwiperContainer } from 'swiper/element';
+import { Swiper } from 'swiper/types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [PricePipe, ButtonComponent],
+  imports: [PricePipe, ButtonComponent, ImageComponent, CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export default class ProductComponent {
   private productService = inject(ProductService);
   private utilService = inject(UtilService);
+  imageIndex = signal<number>(0);
+  @ViewChild('swiper') swiper!: ElementRef<SwiperContainer>;
 
   public productId = input.required<number, string>({
     transform: (value: string) => parseInt(value),
@@ -65,5 +74,24 @@ export default class ProductComponent {
         this.loading.set(LoadingStatus.Error);
       },
     });
+  }
+
+  ngAfterViewInit() {
+    this.swiper.nativeElement.swiper.on(
+      'slideNextTransitionEnd',
+      (swiper: Swiper) => {
+        this.imageIndex.set(swiper.activeIndex);
+      }
+    );
+    this.swiper.nativeElement.swiper.on(
+      'slidePrevTransitionEnd',
+      (swiper: Swiper) => {
+        this.imageIndex.set(swiper.activeIndex);
+      }
+    );
+  }
+
+  slideToImage(imageIndex: number) {
+    this.swiper.nativeElement.swiper.slideTo(imageIndex);
   }
 }
