b6084c0b93ce0a19a22d78769f607918cd561954
joseperu2503 <joseperu2503@gmail.com>
2024-08-06 22:14:56 -0500

    menu user

---
+++ src/app/shared/components/user-button/user-button.component.ts
@@ -0,0 +1,81 @@
+import { Component, inject, signal } from '@angular/core';
+import { UserService } from '../../../features/user/services/user.service';
+import { AuthService } from '../../../features/auth/services/auth.service';
+import { Router } from '@angular/router';
+import { SvgIconComponent } from 'angular-svg-icon';
+import { CommonModule } from '@angular/common';
+import { MatRippleModule } from '@angular/material/core';
+import { NgClickOutsideDirective } from 'ng-click-outside2';
+import { ButtonComponent } from '../button/button.component';
+
+@Component({
+  selector: 'app-user-button',
+  standalone: true,
+  imports: [
+    SvgIconComponent,
+    CommonModule,
+    MatRippleModule,
+    NgClickOutsideDirective,
+    ButtonComponent,
+  ],
+  templateUrl: './user-button.component.html',
+  styleUrl: './user-button.component.scss',
+})
+export class UserButtonComponent {
+  userService = inject(UserService);
+  authService = inject(AuthService);
+  router = inject(Router);
+  showMenu = signal<boolean>(false);
+
+  toggleMenu() {
+    this.showMenu.update((value) => !value);
+  }
+
+  closeMenu() {
+    this.showMenu.set(false);
+  }
+
+  onClickMenuItem(menuItem: MenuItem) {
+    this.closeMenu();
+    menuItem.onPress();
+  }
+
+  menuItems = signal<MenuItem[]>([
+    {
+      icon: 'assets/icons/profile.svg',
+      label: 'Account Information',
+      onPress: () => {},
+    },
+    {
+      icon: 'assets/icons/card.svg',
+      label: 'My Cards',
+      onPress: () => {},
+    },
+    {
+      icon: 'assets/icons/order.svg',
+      label: 'My Orders',
+      onPress: () => {},
+    },
+    {
+      icon: 'assets/icons/box.svg',
+      label: 'My Addresses',
+      onPress: () => {},
+    },
+    {
+      icon: 'assets/icons/lock.svg',
+      label: 'Change Password',
+      onPress: () => {},
+    },
+    {
+      icon: 'assets/icons/settings.svg',
+      label: 'Settings',
+      onPress: () => {},
+    },
+  ]);
+}
+
+interface MenuItem {
+  icon: string;
+  label: string;
+  onPress: Function;
+}
