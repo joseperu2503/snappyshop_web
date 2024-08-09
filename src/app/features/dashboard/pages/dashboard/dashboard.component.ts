3c3d3044c5f398633af4f18c00b5444e5e15911e
joseperu2503 <joseperu2503@gmail.com>
2024-08-08 23:40:46 -0500

    product skeleton

--- src/app/features/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,17 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { ProductItemComponent } from '../../../product/components/product-item/product-item.component';
 import { ProductStore } from '../../../product/stores/product.store';
+import { ProductSkeletonComponent } from '../../../product/components/product-skeleton/product-skeleton.component';
+import { LoadingStatus } from '../../../../core/enums/loading-status.enum';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ProductItemComponent],
+  imports: [ProductItemComponent, ProductSkeletonComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export default class DashboardComponent {
   productStore = inject(ProductStore);
-
+  LoadingStatus = LoadingStatus;
   ngOnInit() {
     this.productStore.getProducts();
   }
---
+++ src/app/features/product/components/product-skeleton/product-skeleton.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-skeleton',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-skeleton.component.html',
+  styleUrl: './product-skeleton.component.scss'
+})
+export class ProductSkeletonComponent {
+
+}
