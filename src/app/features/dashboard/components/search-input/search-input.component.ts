4ac70ed605a179650ea07eb20404a73b7938320e
joseperu2503 <joseperu2503@gmail.com>
2024-08-08 19:01:01 -0500

    search input

--- src/app/features/dashboard/components/search-input/search-input.component.ts
+++ src/app/features/dashboard/components/search-input/search-input.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
 import { SvgIconComponent } from 'angular-svg-icon';
+import { NgClickOutsideDirective } from 'ng-click-outside2';
+import { SearchStore } from '../../../search/stores/search.store';
+import { ImageComponent } from '../../../../shared/components/image/image.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-search-input',
   standalone: true,
-  imports: [SvgIconComponent, SvgIconComponent],
+  imports: [
+    SvgIconComponent,
+    SvgIconComponent,
+    CommonModule,
+    NgClickOutsideDirective,
+    ImageComponent,
+    RouterLink,
+  ],
   templateUrl: './search-input.component.html',
   styleUrl: './search-input.component.scss',
 })
-export class SearchInputComponent {}
+export class SearchInputComponent {
+  showMenu = signal<boolean>(false);
+  searchStore = inject(SearchStore);
+
+  closeMenu() {
+    this.showMenu.set(false);
+  }
+
+  openMenu() {
+    this.showMenu.set(true);
+  }
+
+  changeInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    this.searchStore.changeSearch(input.value);
+  }
+}
